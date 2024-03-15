5af8012c8cbe42769c876ca0291f10d93acdfd0f
ArnaudFonzam <gui.fofou@umi2growcameroun.com>
2024-03-15 12:34:06 +0100

    first pages routing

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="text-3xl font-bold underline">
-    Hello word!
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ pages/events.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="text-3xl font-bold underline">
+        Events Pages
+    </div>
+</template>
